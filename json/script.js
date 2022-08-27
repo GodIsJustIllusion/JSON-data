27dd361cfb7cdbb0cb9961089ff32aa4cac36e10
Igor_A <87186964+GodIsJustIllusion@users.noreply.github.com>
2022-08-27 15:46:50 +0400

    Add files via upload

---
+++ json/script.js
@@ -0,0 +1,91 @@
+let person = {
+    "gender": "female",
+    "name": {
+      "title": "Miss",
+      "first": "Jennie",
+      "last": "Nichols"
+    },
+    "location": {
+      "street": {
+        "number": 8929,
+        "name": "Valwood Pkwy",
+      },
+      "city": "Billings",
+      "state": "Michigan",
+      "country": "United States",
+      "postcode": "63104",
+      "coordinates": {
+        "latitude": "-69.8246",
+        "longitude": "134.8719"
+      },
+      "timezone": {
+        "offset": "+9:30",
+        "description": "Adelaide, Darwin"
+      }
+    },
+    "email": "jennie.nichols@example.com",
+    "login": {
+      "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+      "username": "yellowpeacock117",
+      "password": "addison",
+      "salt": "sld1yGtd",
+      "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+      "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+      "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+    },
+    "dob": {
+      "date": "1992-03-08T15:13:16.688Z",
+      "age": 30
+    },
+    "registered": {
+      "date": "2007-07-09T05:51:59.390Z",
+      "age": 14
+    },
+    "phone": "(272) 790-0888",
+    "cell": "(489) 330-2385",
+    "id": {
+      "name": "SSN",
+      "value": "405-88-3636"
+    },
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/75.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+    },
+    "nat": "US"
+  }
+
+  const body = document.body;
+
+
+  function createPerson (person = {}) {
+    let div = document.createElement('div');
+    div.innerText = "Пол: " + person.gender;
+    body.appendChild(div);
+
+    div = document.createElement('div');
+    div.innerText = "Имя: " + person.name.title + " " + person.name.first + " " + person.name.last;
+    body.appendChild(div);
+
+    div = document.createElement('div');
+    div.innerText ="Улица: " + person.location.street.number + " " + person.location.street.name;
+    body.appendChild(div);
+
+    div = document.createElement('div');
+    div.innerText = "Город: " + person.location.city + ", " + "\nШтат: " + person.location.state + ", " + "\nСтрана: " + person.location.country + ", " + "\nПочтовый индекс: " + person.location.postcode + ", " + "\nКоординаты: " + person.location.coordinates.latitude + " " + person.location.coordinates.longitude;
+    body.appendChild(div);
+
+    div = document.createElement('div');
+    div.innerText = "Почта: " + person.email;
+    body.appendChild(div);
+
+    div = document.createElement('div');
+    div.innerText = "Логин: \n" + "\tID: " + person.login.uuid + "\n\tUsername: " + person.login.username + "\n\tPassword: " + person.login.password + "\n\tSalt: " + person.login.salt + "\n\tMD5: " + person.login.md5 + "\n\tSha1: " + person.login.sha1 + "\n\tSha256: " + person.login.sha256;
+    body.appendChild(div);
+
+    div = document.createElement('div');
+    div.innerText = "Дата рождения: " + person.dob.date + "";
+    body.appendChild(div);
+  }
+
+  createPerson(person);
